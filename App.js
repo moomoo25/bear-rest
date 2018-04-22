de26ecb9089fbc239009d4b8bc1b2fd0e7c72fa7
moomoo25 <mooja_25@hotmail.com>
2018-04-22 22:59:49 +0700

    list bears
    ทวีวัฒน์ สุนทรธนศาสตร์ 5710110171

--- App.js
+++ App.js
@@ -1,9 +1,24 @@
 import React, { Component } from 'react';
-
+import axios from 'axios';
 class App extends Component {
-  render() {
+    constructor(props){
+        super(props)
+        this.state={data:[]};
+    }
+    componentDidMount(){
+        axios.get('http://localhost:8000/api/bears')
+        .then(result=>{
+            this.setState({data:result.data})
+        })
+    }
+    render() {
+        let bears=this.state.data;
     return (
-      <div>Hello Myworld!!!</div>
+      <div>
+        {
+            bears.map(bear=><div key={bear.id}>{bear.name}</div>)
+        }
+        </div>
     );
   }
 }
