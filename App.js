e748e4bafae1a9ae9b7779f8923b996f89fc32c6
moomoo25 <mooja_25@hotmail.com>
2018-04-22 22:21:47 +0700

    ิิbear rest

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class App extends Component {
   render() {
     return (
-      <div>Hello world!!!</div>
+      <div>Hello Myworld!!!</div>
     );
   }
 }
