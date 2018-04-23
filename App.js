0bbe9c081d9e134edeef8f2ae911c1b52f0fcda6
moomoo25 <mooja_25@hotmail.com>
2018-04-23 21:31:11 +0700

    seperate action and reducer from AppJs
    5710110171 ทวีวัฒน์ สุนทรธนศาสตร์

--- App.js
+++ App.js
@@ -1,27 +1,13 @@
 import React, { Component } from 'react';
-import axios from 'axios';
-import {createStore,applyMiddleware,combineReducers} from 'redux';
-import thunk from 'redux-thunk';
-const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
-let bearReducer = function(state=[],action){
-    if(action){
-        if(action.type === 'FETCH_BEAR'){
-            return action.payload
-        }
-    }
-    return state
 
-}
-    let store = createStoreWithMiddleware(combineReducers ({
-        bear:bearReducer}))
-    let fetchBearActionCreator =function(){
-        return (dispatch)=>{
-            axios.get('http://localhost:8000/api/bears')
-     .then(result=>{
-        dispatch({type:'FETCH_BEAR',payload:result.data})
-     })
-        }
-    }
+import {createStore,applyMiddleware} from 'redux';
+import thunk from 'redux-thunk';
+import reducer from './reducer';
+import {fetchBear} from './action';
+const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
+
+    let store = createStoreWithMiddleware(reducer)
+
 class App extends Component {
 
     constructor(props){
@@ -32,7 +18,7 @@ class App extends Component {
         store.subscribe(()=>{
             this.setState({data:store.getState().bear})
         })
-        store.dispatch(fetchBearActionCreator());
+        store.dispatch(fetchBear());
     }
     render() {
         let bears=this.state.data;
---
+++ reducer.js
@@ -0,0 +1,15 @@
+// JavaScript source code
+import {combineReducers} from 'redux';
+let bearReducer = function(state=[],action){
+    if(action){
+        if(action.type === 'FETCH_BEAR'){
+            return action.payload
+        }
+    }
+    return state
+
+}
+  let reducer=  combineReducers ({
+      bear:bearReducer})
+
+      export default reducer;
