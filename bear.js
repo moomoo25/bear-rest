5b3443371f34ace1ec1bebc99c3d7381ab73b71e
moomoo25 <mooja_25@hotmail.com>
2018-04-26 17:04:27 +0700

    Add files via upload
    ทวีวัฒน์ สุนทรธนศาสตร์ 5710110171

--- bear.js
+++ bear.js
@@ -9,14 +9,20 @@ var bears = [
 var cors = require('cors');
 router.route('/bears')
 	.get(function (req, res) {
-	res.send(bears);
-})
+	    res.send(bears);
+	})
 .post(function(req, res) {
-var bear = {};
-bear.name = req.body.name;
-bears.push(bear);
-res.json({ message: 'Bear created!' });
-});
+    var bear = {};
+    bear.name = req.body.name;
+    bears.push(bear);
+    res.json({ message: 'Bear created!' });
+})
+ router.route('/bear/:id')
+    .delete(function(req,res)
+{
+    bears=bears.filter(b=>b.id!=req.params.id )
+    res.json({ message: 'Bear deleted' });
+})
 app.use(cors())
 // all of our routes will be prefixed with /api
 app.use('/api', bodyParser.json(), router);
